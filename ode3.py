bb0d807a121a022c7501bf889f8b56729b8619d7
Tom Fletcher <tf810@ic.ac.uk>
2013-12-23 00:06:39 +0000

    comments and analytical solution comparison for weird non-diverging stuff outside horizon

--- ode3.py
+++ ode3.py
@@ -5,6 +5,7 @@ import math
 global k
 
 #--------------------------------------------------------------------------------------------------------------------------------
+## Define Parameter Class so don't have to pass multiple variables between functions and can have preset values ie for inflation
 # class ParamsType(object):
 # 	w = -1
 # 	kspace = numpy.linspace(1,10,100)
@@ -29,9 +30,13 @@ def RK4(k, n_inside, n_outside, num_steps):
 	times = numpy.linspace(n_inside,n_outside,num_steps)
 	h = times[1]-times[0]
 
-	data = numpy.zeros((len(times),3))
+	data = numpy.zeros((len(times),4))
 	xn = 1/(math.sqrt(2*k))*math.cos(k*n_inside)
 	yn = -k/(math.sqrt(2*k))*math.sin(k*n_outside)
+
+	# RK4 method of integrating ODE where 2nd order mukhanov-sasaki equation has been re-written as 2 1st order coupled ODEs using: x = v, y = dv/dn
+	# --> ODE_1: dx/dn = y
+	# --> ODE_2: dy/dn = -[(k^2)+(2/n^2)]x
 
 	for idx, time in enumerate(times):
 		n = times[idx]
@@ -56,6 +61,9 @@ def RK4(k, n_inside, n_outside, num_steps):
 			yn = yn + (1.0/6.0)*(yk1 + (2*yk2) + (2*yk3) + yk4)
 
 			data[idx,0], data[idx,1], data[idx,2] = n, xn, yn
+
+			# Trying to plot analytical solution for de-Sitter space ('http://www.damtp.cam.ac.uk/user/db275/Cosmology/Chapter5.pdf' eqn 5.1.23 but when alpha=beta=1)
+			data[idx,3] = (2/math.sqrt(2*k))*(math.cos(k*n)-(1/(k*n))*math.sin(k*n))
 	return data
 #-----------------------------------------------------------------------------------------------------
 k = 1
@@ -64,24 +72,28 @@ k = 1
 kspace = numpy.linspace(1,10,100)
 n_inside = -1000/(numpy.amin(kspace))
 n_outside = -1/(1000*numpy.amax(kspace))
-num_steps = 10000
+num_steps = 100000
 times = numpy.linspace(n_inside,n_outside,num_steps)
 
 w = -1
 coeff = 2/((3*w)+1)
 C = coeff*(coeff-1)
 
-
+# odeint method for comparison with RK4 and analytical methods
 xinit = numpy.array([1/(math.sqrt(2*k))*math.cos(k*n_inside), -k/(math.sqrt(2*k))*math.sin(k*n_inside)])
 odemethod = odeint(deriv,xinit,times) 
 
 data = RK4(k,n_inside,n_outside,num_steps)
 
 plt.plot(data[:,0], data[:,1], color = 'r')
+# plt.scatter(data[:,0], data[:,1], color = 'r')
 plt.plot(data[:,0], odemethod[:,0], color = 'c')
-plt.axvline(x=-1/1, ymin=-99, ymax=999, color='m')
+# plt.scatter(data[:,0], odemethod[:,0], color = 'c')
+plt.plot(data[:,0], data[:,3], color = 'm')
+plt.axvline(x=-1/k, ymin=-99, ymax=999, color='m')
+plt.axvline(x=-1)
 # plt.axvline(x=-1/freeze_time,ymin=-999,ymax=999,color='m')
 plt.axhline(y=0,xmin=-9999,xmax=9999,color='k')
-# plt.xlim(-10,0)
+plt.xlim(-1.5,0)												# Comment in to see how it behaves outside horizon (should go v ~ a but doesnt change until the final point regardless of step size)
 # plt.ylim(-1,1)
 plt.show()
