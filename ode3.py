55f20e3bc3592d2ae6fd1aaf80981b75e215c428
tom <tomftom99@gmail.com>
2014-03-04 17:16:38 +0000

    trying to add a seperate thing to work out the power spectrum for jacobi elliptic functions not yet finished as the deriv function doesn't quite work properly yet

--- ode3.py
+++ ode3.py
@@ -87,128 +87,179 @@ def kspectrum(kspace, n_a, n_b, nt):
 		
 		# data[i,3] = math.log(k*k*k*data[i,2]*data[i,2]*time[999]*time[999])
 #--------------------------------------------------------------------------------------------------------------------------------
-def JacobiElipa(n_a,n_b,num_steps):
+def JacobiElipa():
+	kspace = numpy.linspace(1,100,10)
+	n_inside = -100/(numpy.amin(kspace))
+	n_outside = -1/(1000*numpy.amax(kspace))
+	num_steps = 100000
+	h = ((n_outside - n_inside)/num_steps)
+	#-------------------------------------
 	dn = mpmath.ellipfun('dn')
 	data = []
-	xpts = numpy.linspace(n_a,n_b,num_steps)
+	xpts = numpy.linspace(n_inside,n_outside,num_steps)
 	for x in xpts:
 		a = dn(x, 1)
-		print x,a
 		data.append(a)
-	return data
+	return data,h
+#--------------------------------------------------------------------------------------------------------------------------------
+def derivjacobi(y,timestep):
+	avals,h = JacobiElipa()
+	seconderiv = (avals[timestep] - 2*avals[timestep-1] + avals[timestep-2])/(h*h)
+	addoa = seconderiv/a
+	a = -((k*k)-addoa)
+	return numpy.array([ y[1], a*y[0] ])
 #--------------------------------------------------------------------------------------------------------------------------------
 params = ParamsType()
 
-w_list, ns_list = [], []
+#include option to select between normal perfect fluid equations or other alternative methods
+#option 1 = normal method, 2 = Jacobi Elliptic scale factor
+selection = raw_input('Perfect Fluid: [1], or Jacobi Elliptic [2]')
 
-w_llim, w_ulim, w_steps = -3,3,50
-wspace = numpy.linspace(w_llim,w_ulim,w_steps)
-for w_val in wspace:
-	w = w_val
-	print w
-	if -0.34 < w < -0.32:
-		print '@@@@@@@@@@@@@@@@@ in the condition'
-		w_list.append(w)
-		ns_list.append(False)
-	else:
-		kspace = numpy.linspace(1,100,10)
-		n_inside = -100/(numpy.amin(kspace))
-		n_outside = -1/(1000*numpy.amax(kspace))
-		# if w > 1/3:
-		# 	n_inside, n_outside = -n_outside, -n_inside
-		num_steps = 100000
-		times = numpy.linspace(n_inside,n_outside,num_steps)
+if selection == 1:
+	print 'Perfect Fluid Selected'
 
-		# Use w to work out: {numerator in M-S eqn, a dependence on eta}
-		# Also (in general) the power eta (conformal time) is raised to in the equation for a is calculated (a_power)
-		a_power = 2/((3*w)+1)
-		C = a_power*(a_power-1)
+	w_list, ns_list = [], []
 
-		''' Comment this bit back in to iterate for just one k value and see the point where the solution blows up outside the horizon
-		k=1
-		xinit = numpy.array([1/(math.sqrt(2*k))*math.cos(k*n_inside), -k/(math.sqrt(2*k))*math.sin(k*n_inside)])
-		odemethod = odeint(deriv,xinit,times) 
-		data = RK4(k,n_inside,n_outside,num_steps)
-		plotgraphs(data, odemethod) '''
+	w_llim, w_ulim, w_steps = -3,3,50
+	wspace = numpy.linspace(w_llim,w_ulim,w_steps)
+	for w_val in wspace:
+		w = w_val
+		print w
+		if -0.34 < w < -0.32:
+			print '@@@@@@@@@@@@@@@@@ in the condition'
+			w_list.append(w)
+			ns_list.append(False)
+		else:
+			kspace = numpy.linspace(1,100,10)
+			n_inside = -100/(numpy.amin(kspace))
+			n_outside = -1/(1000*numpy.amax(kspace))
+			# if w > 1/3:
+			# 	n_inside, n_outside = -n_outside, -n_inside
+			num_steps = 100000
+			times = numpy.linspace(n_inside,n_outside,num_steps)
 
-		data = numpy.zeros((len(kspace),4))
+			# Use w to work out: {numerator in M-S eqn, a dependence on eta}
+			# Also (in general) the power eta (conformal time) is raised to in the equation for a is calculated (a_power)
+			a_power = 2/((3*w)+1)
+			C = a_power*(a_power-1)
 
-		for i in range(0,len(kspace)):
-			k = kspace[i]
-			time = numpy.linspace(n_inside,n_outside,num_steps) 
-			# time = numpy.logspace(n_a,n_b,num=100) 
-			# print time
+			''' Comment this bit back in to iterate for just one k value and see the point where the solution blows up outside the horizon
+			k=1
 			xinit = numpy.array([1/(math.sqrt(2*k))*math.cos(k*n_inside), -k/(math.sqrt(2*k))*math.sin(k*n_inside)])
-			x = odeint(deriv,xinit,time) 
-			yinit = numpy.array([-1/(math.sqrt(2*k))*math.sin(k*n_inside), -k/(math.sqrt(2*k))*math.cos(k*n_inside)])
-			y = odeint(deriv,yinit,time)
-			x_len = int(len(x))-3
-			data[i,0] = math.log(k)
-			data[i,1] = math.sqrt(x[5,0]*x[5,0] + y[5,0]*y[5,0]) 
-			data[i,2] = math.sqrt(x[x_len,0]*x[x_len,0] + y[x_len,0]*y[x_len,0]) 
-			data[i,3] = math.log(k*k*k*data[i,2]*data[i,2]*math.pow(abs(time[x_len]),a_power)*math.pow(abs(time[x_len]),a_power))		
+			odemethod = odeint(deriv,xinit,times) 
+			data = RK4(k,n_inside,n_outside,num_steps)
+			plotgraphs(data, odemethod) '''
 
-		#--------------------------------------------------------------------------------------------------------------------------------
-		coefficients = numpy.polyfit(data[:,0], data[:,3], 1)
-		polynomial = numpy.poly1d(coefficients)
-		ys = polynomial(data[:,0])
-		# print polynomial
-		w_list.append(w)
-		ns_list.append(polynomial[1])
+			data = numpy.zeros((len(kspace),4))
 
-# plt.scatter(w_list, ns_list)
-# plt.show()
+			for i in range(0,len(kspace)):
+				k = kspace[i]
+				time = numpy.linspace(n_inside,n_outside,num_steps) 
+				# time = numpy.logspace(n_a,n_b,num=100) 
+				# print time
+				xinit = numpy.array([1/(math.sqrt(2*k))*math.cos(k*n_inside), -k/(math.sqrt(2*k))*math.sin(k*n_inside)])
+				x = odeint(deriv,xinit,time) 
+				yinit = numpy.array([-1/(math.sqrt(2*k))*math.sin(k*n_inside), -k/(math.sqrt(2*k))*math.cos(k*n_inside)])
+				y = odeint(deriv,yinit,time)
+				x_len = int(len(x))-3
+				data[i,0] = math.log(k)
+				data[i,1] = math.sqrt(x[5,0]*x[5,0] + y[5,0]*y[5,0]) 
+				data[i,2] = math.sqrt(x[x_len,0]*x[x_len,0] + y[x_len,0]*y[x_len,0]) 
+				data[i,3] = math.log(k*k*k*data[i,2]*data[i,2]*math.pow(abs(time[x_len]),a_power)*math.pow(abs(time[x_len]),a_power))		
 
-# add 1 to each element to convert from ns-1 to ns
-ns_list = [x+1 for x in ns_list]
+			#--------------------------------------------------------------------------------------------------------------------------------
+			coefficients = numpy.polyfit(data[:,0], data[:,3], 1)
+			polynomial = numpy.poly1d(coefficients)
+			ys = polynomial(data[:,0])
+			# print polynomial
+			w_list.append(w)
+			ns_list.append(polynomial[1])
 
-# find ns values of interest closte to 0.96
-ns_scale_invariant, w_scale_invariant = [], []
-for a,b in enumerate(ns_list):
-	if 0.9 < b < 1.06:
-		ns_scale_invariant.append(b)
-		w_scale_invariant.append(w_list[a])
-
-wspace = numpy.linspace(w_llim, w_ulim,10000)
-real = []
-for w in wspace:
-	if -0.33333<w<1.0:
-		real.append(4+((3*(w-1))/(1+(3*w))))
-	else:
-		real.append(4-((3*(w-1))/(1+(3*w))))
-
-# Plot w vs ns for the region of w being studied
-plt.plot(w_list, ns_list)
-plt.xlabel('w')
-plt.ylabel('ns')
-plt.axhline(y=0.96,xmin=-9999,xmax=9999)
-if len(ns_scale_invariant) > 0:
-	plt.scatter(w_scale_invariant, ns_scale_invariant, color = 'r')
-plt.plot(wspace, real, color = 'k')
-plt.savefig('~Results' '{' + str(w_llim) + ',' + str(w_ulim) + '}' + ',' + str(w_steps) + '_' + 'steps' + '_' + 'graph.png')
-plt.ylim(-10,10)
-plt.show()
-
-# save data to text file so don't have to run again
-text_data = numpy.vstack((w_list,ns_list))
-text_data = numpy.reshape(text_data,[len(w_list),2])
-numpy.savetxt('~Results' '{' + str(w_llim) + ',' + str(w_ulim) + '}' + ',' + str(w_steps) + '_' + 'steps' + '_' + 'data.txt', text_data)	
-
-
-
-	# f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=False)
-	# ax1.set_title('inside horizon')
-	# ax1.plot(data[:,0],data[:,1])
-	# ax2.set_title('outside horizon')
-	# ax2.plot(data[:,0],data[:,2], color = 'r')
-	# ax3.set_title('outside horizon powe spec')
-	# ax3.plot(data[:,0],data[:,3], color = 'g')
-	# ax3.plot(data[:,0],ys)
-	# # plt.xlim(1,2.5)
+	# plt.scatter(w_list, ns_list)
 	# plt.show()
 
+	# add 1 to each element to convert from ns-1 to ns
+	ns_list = [x+1 for x in ns_list]
 
-# a_pow = calc_a_dependence(1/3)
-# print a_pow
+	# find ns values of interest closte to 0.96
+	ns_scale_invariant, w_scale_invariant = [], []
+	for a,b in enumerate(ns_list):
+		if 0.9 < b < 1.06:
+			ns_scale_invariant.append(b)
+			w_scale_invariant.append(w_list[a])
 
-# test_w_vs_a(-10,10)
+	wspace = numpy.linspace(w_llim, w_ulim,10000)
+	real = []
+	for w in wspace:
+		if -0.33333<w<1.0:
+			real.append(4+((3*(w-1))/(1+(3*w))))
+		else:
+			real.append(4-((3*(w-1))/(1+(3*w))))
+
+	# Plot w vs ns for the region of w being studied
+	plt.plot(w_list, ns_list)
+	plt.xlabel('w')
+	plt.ylabel('ns')
+	plt.axhline(y=0.96,xmin=-9999,xmax=9999)
+	if len(ns_scale_invariant) > 0:
+		plt.scatter(w_scale_invariant, ns_scale_invariant, color = 'r')
+	plt.plot(wspace, real, color = 'k')
+	plt.savefig('~Results' '{' + str(w_llim) + ',' + str(w_ulim) + '}' + ',' + str(w_steps) + '_' + 'steps' + '_' + 'graph.png')
+	plt.ylim(-10,10)
+	plt.show()
+
+	# save data to text file so don't have to run again
+	text_data = numpy.vstack((w_list,ns_list))
+	text_data = numpy.reshape(text_data,[len(w_list),2])
+	numpy.savetxt('~Results' '{' + str(w_llim) + ',' + str(w_ulim) + '}' + ',' + str(w_steps) + '_' + 'steps' + '_' + 'data.txt', text_data)	
+
+
+
+		# f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=False)
+		# ax1.set_title('inside horizon')
+		# ax1.plot(data[:,0],data[:,1])
+		# ax2.set_title('outside horizon')
+		# ax2.plot(data[:,0],data[:,2], color = 'r')
+		# ax3.set_title('outside horizon powe spec')
+		# ax3.plot(data[:,0],data[:,3], color = 'g')
+		# ax3.plot(data[:,0],ys)
+		# # plt.xlim(1,2.5)
+		# plt.show()
+
+
+	# a_pow = calc_a_dependence(1/3)
+	# print a_pow
+
+	# test_w_vs_a(-10,10)
+else:
+	print ' Jacobi Elliptic selected'
+
+	kspace = numpy.linspace(1,100,10)
+	n_inside = -100/(numpy.amin(kspace))
+	n_outside = -1/(1000*numpy.amax(kspace))
+	num_steps = 100000
+	times = numpy.linspace(n_inside,n_outside,num_steps)
+	scalefactors,h = JacobiElipa()
+
+	data = numpy.zeros((len(kspace),4))
+
+	for i in range(0,len(kspace)):
+		print i
+		k = kspace[i]
+		time = numpy.linspace(n_inside,n_outside,num_steps) 
+		xinit = numpy.array([1/(math.sqrt(2*k))*math.cos(k*n_inside), -k/(math.sqrt(2*k))*math.sin(k*n_inside)])
+		x = odeint(derivjacobi,xinit,time) 
+		yinit = numpy.array([-1/(math.sqrt(2*k))*math.sin(k*n_inside), -k/(math.sqrt(2*k))*math.cos(k*n_inside)])
+		y = odeint(derivjacobi,yinit,time)
+		x_len = int(len(x))-3
+		data[i,0] = math.log(k)
+		data[i,1] = math.sqrt(x[5,0]*x[5,0] + y[5,0]*y[5,0]) 
+		data[i,2] = math.sqrt(x[x_len,0]*x[x_len,0] + y[x_len,0]*y[x_len,0]) 
+		data[i,3] = math.log((k*k*k*data[i,2]*data[i,2])/(scalefactors[x_len]*scalefactors[x_len]))
+	#--------------------------------------------------------------------------------------------------------------------------------
+	coefficients = numpy.polyfit(data[:,0], data[:,3], 1)
+	polynomial = numpy.poly1d(coefficients)
+	print polynomial[1]
+
+
+
